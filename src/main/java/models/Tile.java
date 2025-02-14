90da6aec893e5bd915fbe797347525427eb573ef
Erlend <erlesu@stud.ntnu.no>
2025-02-14 14:45:15 +0100

    implement basic methods in Tile class

--- src/main/java/models/Tile.java
+++ src/main/java/models/Tile.java
@@ -1,5 +1,22 @@
 package models;
 
-public class Tile {
+import interfaces.TileAction;
 
+public class Tile {
+  private int tileId;
+  private Tile nextTile;
+  private TileAction landAction = new LadderAction(5, "Test");
+
+
+  public void landPlayer(Player player) {
+    landAction.preform(player);
+  }
+
+  public int getTileId() {
+    return tileId;
+  }
+
+  public Tile getNextTile() {
+    return nextTile;
+  }
 }
