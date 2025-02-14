0fc16bd554dd58d4cf81b87bb20c11ec24776889
Erlend <erlesu@stud.ntnu.no>
2025-02-14 14:49:23 +0100

    implement basic method headers in Player class

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -1,5 +1,32 @@
 package models;
 
-public class Player {
+import controllers.BoardGame;
 
+public class Player {
+  private String name;
+  private Tile currentTile;
+
+  Player(String name, BoardGame game){
+    setName(name);
+  }
+
+  public void move(int steps) {
+    Tile tileMovedTo = currentTile;
+    for (int i = 0; i < steps; i++) {
+      tileMovedTo = tileMovedTo.getNextTile();
+    }
+    placeOnTile(tileMovedTo);
+  }
+
+  public void placeOnTile(Tile tile) {
+    setCurrentTile(tile);
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setCurrentTile(Tile currentTile) {
+    this.currentTile = currentTile;
+  }
 }
