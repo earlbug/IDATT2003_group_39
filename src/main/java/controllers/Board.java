981157ee745d89455c1f8a085fca9bb3717d035e
Erlend <erlesu@stud.ntnu.no>
2025-02-24 17:34:41 +0100

    feat(Board): implement basic appropriate methods

--- src/main/java/controllers/Board.java
+++ src/main/java/controllers/Board.java
@@ -1,5 +1,28 @@
 package controllers;
 
+import java.util.Map;
+import models.Tile;
+
+/**
+ * Represents a board in ladder game. It has the responsibility of managing the tiles of the game.
+ *
+ * @version 0.1.0
+ * @author Erlend Sundsdal
+ * @since 0.1.0
+ */
 public class Board {
+  private Map<Integer, Tile> tiles;
+
+  public Board(){}
+
+  public Tile getTile(int tileId) {
+    return tiles.get(tileId);
+  }
+
+  public void addTile(Tile tile) {
+    tiles.put(tiles.size(), tile);
+  }
+
+
 
 }
