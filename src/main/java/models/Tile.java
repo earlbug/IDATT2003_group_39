466fb382a1a8f32769c0a9eaa40bbc34cd9b2b53
Erlend <erlesu@stud.ntnu.no>
2025-02-24 17:00:49 +0100

    feat(Tile): make Tile Class with basic appropriate methods

--- src/main/java/models/Tile.java
+++ src/main/java/models/Tile.java
@@ -2,21 +2,54 @@ package models;
 
 import interfaces.TileAction;
 
+/**
+ * Represents a tile in the ladder game. A TileActions is stored as a class variable, and decides
+ * what happens when a player lands on the Tile.
+ *
+ * @version 0.1.0
+ * @author Erlend Sundsdal
+ * @since 0.1.0
+ */
 public class Tile {
   private int tileId;
   private Tile nextTile;
-  private TileAction landAction = new LadderAction(5, "Test");
+  private TileAction landAction;
 
+  public Tile(int tileId) {
+    setTileId(tileId);
+  }
 
+  /**
+   * Preforms the stored <code>TileAction</code> on the player parameter
+   *
+   * @param player the player whom the action shall be preformed on
+   */
   public void landPlayer(Player player) {
     landAction.preform(player);
   }
+
+  public void leavePlayer(Player player) {}
+
 
   public int getTileId() {
     return tileId;
   }
 
+
+
   public Tile getNextTile() {
     return nextTile;
+  }
+
+  public void setTileId(int tileId) {
+    this.tileId = tileId;
+  }
+
+  public void setNextTile(Tile nextTile) {
+    this.nextTile = nextTile;
+  }
+
+  public void setLandAction(TileAction landAction) {
+    this.landAction = landAction;
   }
 }
