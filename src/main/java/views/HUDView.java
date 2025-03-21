01c0805a0019a1578731260a38e904622ebffea8
torfoz <tord.fosse@gmail.com>
2025-03-21 10:37:28 +0100

    chore: refactor to clean up code

--- src/main/java/views/HUDView.java
+++ src/main/java/views/HUDView.java
@@ -1,7 +1,6 @@
 package views;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import controllers.Dice;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -9,25 +8,49 @@ import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+import models.Player;
+import views.container.MainView;
 
-public class HUDView {
+public class HUDView extends VBox{
+
+  private final BoardView boardView;
+  private final Dice dice;
 
   final int MAX_WITH = 300;
 
-  VBox root = new VBox();
-  VBox playerContainer = new VBox();
-  VBox diceContainer = new VBox();
+  private final VBox playerContainer;
+  private final VBox diceContainer;
 
-  Text playerTitle = new Text("Player");
-  Text playerNumber = new Text(Integer.toString(1));
+  private Text playerTitle;
+  private Text playerNumber;
 
-  Region spacer = new Region();
+  private Region spacer;
 
-  Button rollDiceButton = new Button();
-  Text diceNumber = new Text();
+  private Button rollDiceButton;
+  private Text diceNumber;
 
-  public HUDView() {
+  private Button testPlayerPos;
+  private Player player;
 
+  public HUDView(MainView mainView, BoardView boardView) {
+    this.boardView = boardView;
+    this.dice = new Dice(1);
+    this.playerContainer = new VBox();
+    this.diceContainer = new VBox();
+
+    this.playerTitle = new Text("Player");
+    this.playerNumber = new Text(Integer.toString(1));
+
+    this.spacer = new Region();
+    this.rollDiceButton = new Button();
+    this.diceNumber = new Text();
+
+    this.testPlayerPos = new Button("TestPos");
+
+    initialize();
+  }
+
+  private void initialize(){
     playerTitle.getStyleClass().add("player-title");
     playerTitle.setTextAlignment(TextAlignment.CENTER);
 
@@ -37,32 +60,30 @@ public class HUDView {
     playerContainer.getStyleClass().add("player-container");
     playerContainer.setAlignment(Pos.CENTER);
     playerContainer.setMaxWidth(MAX_WITH);
-    playerContainer.getChildren().addAll(playerTitle, playerNumber);
+    playerContainer.getChildren().setAll(playerTitle, playerNumber);
 
     spacer.setMinHeight(20);
 
     rollDiceButton.getStyleClass().add("roll-dice-button");
     rollDiceButton.setAlignment(Pos.CENTER);
-    rollDiceButton.setOnAction(new EventHandler<ActionEvent>() {
-      @Override
-      public void handle(ActionEvent actionEvent) {
-        int diceResult = (int) (Math.random() * 6) + 1;
-        diceNumber.setText(Integer.toString(diceResult));
-      }
+    rollDiceButton.setOnAction(actionEvent -> {
+      dice.rollAllDice();
     });
+
+    testPlayerPos.setOnAction(actionEvent -> boardView.movePlayerToTile(5));
 
     diceNumber.getStyleClass().add("dice-number");
 
     diceContainer.getStyleClass().add("dice-container");
     diceContainer.setAlignment(Pos.CENTER);
     diceContainer.setMaxWidth(MAX_WITH);
-    diceContainer.getChildren().addAll(diceNumber,rollDiceButton);
+    diceContainer.getChildren().setAll(diceNumber, rollDiceButton, testPlayerPos);
 
-    root.setPadding(new Insets(10));
-    root.getChildren().addAll(playerContainer, spacer, diceContainer);
+    this.setPadding(new Insets(10));
+    this.getChildren().setAll(playerContainer, spacer, diceContainer);
   }
 
   public VBox getView() {
-    return root;
+    return this;
   }
 }
