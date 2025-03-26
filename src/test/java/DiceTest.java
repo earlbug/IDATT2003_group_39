6c4bf56ae090752424cb6bf818de10bed666970d
torfoz <tord.fosse@gmail.com>
2025-03-26 15:34:21 +0100

    Junit test for Dice

---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import controllers.Dice;
+import org.junit.jupiter.api.Test;
+
+public class DiceTest {
+
+  private Dice dice = new Dice(5);
+
+  //TODO
+  @Test
+  public void testGetDieValue(){
+    dice.getDieValue(0);
+  }
+
+  //TODO
+  @Test
+  public void testRollAllDice(){
+
+  }
+}
