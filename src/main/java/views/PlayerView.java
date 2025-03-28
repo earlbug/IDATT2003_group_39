419bd521aff84e7d577eb49843f320632c4455c1
torfoz <tord.fosse@gmail.com>
2025-03-28 15:31:51 +0100

    Add JavaDoc

--- src/main/java/views/PlayerView.java
+++ src/main/java/views/PlayerView.java
@@ -4,18 +4,31 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import models.Player;
 
-
-public class PlayerView extends Rectangle{
+/**
+ * <h3>Represents the view of a player</h3>
+ *
+ * <p>Sets the style of a single player.
+ *
+ * @author Tord Fosse
+ * @since 0.1.0
+ */
+public class PlayerView extends Rectangle {
 
   private final Color color;
 
+  /**
+   * Constructs a new view of the player.
+   *
+   * @param player the given player to create
+   * @param color  the color of the player
+   */
   public PlayerView(Player player, Color color) {
     this.color = color;
 
     initialize();
   }
 
-  private void initialize(){
+  private void initialize() {
     double playerSize = 40;
     this.setHeight(playerSize);
     this.setWidth(playerSize);
@@ -23,7 +36,12 @@ public class PlayerView extends Rectangle{
     this.getStyleClass().add("player-avatar");
   }
 
-  public Rectangle getView(){
+  /**
+   * Get the players' avatar.
+   *
+   * @return a rectangle representing the player
+   */
+  public Rectangle getView() {
     return this;
   }
 }
