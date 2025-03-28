f13493537e53ce1fc2096c9f411b3589e4adcf3c
torfoz <tord.fosse@gmail.com>
2025-03-28 15:32:06 +0100

    Add JavaDoc

--- src/main/java/models/Tile.java
+++ src/main/java/models/Tile.java
@@ -4,24 +4,33 @@ import interfaces.TileAction;
 import models.validators.ArgumentValidator;
 
 /**
- * Represents a tile in the ladder game. A TileActions is stored as a class variable, and decides
- * what happens when a player lands on the Tile.
+ * <h3>Represents a tile</h3>
  *
- * @version 0.1.0
+ * <p>A tile is given by a tileId, the next tile and a TileAction. A TileActions is stored as a
+ * class variable, and decides what happens when a player lands on the Tile.
+ *
  * @author Erlend Sundsdal
+ * @author Tord Fosse
+ * @version 0.1.0
  * @since 0.1.0
  */
 public class Tile {
+
   private int tileId;
   private Tile nextTile;
   private TileAction landAction;
 
+  /**
+   * Constructs a new Tile.
+   *
+   * @param tileId the ID of the tile.
+   */
   public Tile(int tileId) {
     setTileId(tileId);
   }
 
   /**
-   * Preforms the stored <code>TileAction</code> on the player parameter
+   * Preforms the stored <code>TileAction</code> on the player parameter.
    *
    * @param player the player whom the action shall be preformed on
    */
@@ -29,32 +38,63 @@ public class Tile {
     landAction.perform(player);
   }
 
-  public void leavePlayer(Player player) {}
+  public void leavePlayer(Player player) {
+  }
 
 
+  /**
+   * Gets the ID from tile given tile.
+   *
+   * @return the ID of the tile.
+   */
   public int getTileId() {
     return tileId;
   }
 
+  /**
+   * Gets the next tile after the current tile.
+   *
+   * @return the next tile.
+   */
   public Tile getNextTile() {
     return nextTile;
   }
 
+  /**
+   * Sets the ID of a tile.
+   *
+   * @param tileId the ID given to the tile.
+   */
   public void setTileId(int tileId) {
     ArgumentValidator.tileSetTileIdValidator(tileId);
     this.tileId = tileId;
   }
 
+  /**
+   * Sets the next tile after the current tile.
+   *
+   * @param nextTile the next tile.
+   */
   public void setNextTile(Tile nextTile) {
     ArgumentValidator.tileSetNextTileValidator(nextTile);
     this.nextTile = nextTile;
   }
 
+  /**
+   * Sets an action on the tile that activates when a player lands on the tile.
+   *
+   * @param landAction the action of the tile.
+   */
   public void setLandAction(TileAction landAction) {
     ArgumentValidator.tileSetLandActionValidator(landAction);
     this.landAction = landAction;
   }
 
+  /**
+   * Gets the land action of the tile.
+   *
+   * @return action when a player lands on the tile.
+   */
   public TileAction getLandAction() {
     return landAction;
   }
