120d3b543a5b16c324db4cd482f638277e765acf
torfoz <tord.fosse@gmail.com>
2025-03-28 15:26:37 +0100

    Initializes the view in the constructor. Add methods for getting board and HUD view.

--- src/main/java/views/container/GameView.java
+++ src/main/java/views/container/GameView.java
@@ -1,37 +1,74 @@
 package views.container;
 
-import controllers.BoardGame;
 import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import views.BoardView;
-import views.HUDView;
+import views.HudView;
 
-public class GameView extends HBox{
+/**
+ * <h3>Represents the view of the game</h3>
+ *
+ * <p>Contains the view of the board and the HUD.
+ *
+ * @author Tord Fosse
+ * @since 0.1.0
+ */
+public class GameView extends HBox {
+
   private final HBox boardContainer;
   private final StackPane hudContainer;
 
   private final BoardView boardView;
-  private final HUDView hudView;
+  private final HudView hudView;
 
-  private final BoardGame boardGame = new BoardGame();
-
-  public GameView(){
+  /**
+   * Constructs a new view of the game. Creates containers to hold the board and HUD.
+   */
+  public GameView() {
     boardContainer = new HBox();
     hudContainer = new StackPane();
 
-    this.boardView = new BoardView(this, boardGame.getBoard());
-    this.hudView = new HUDView(boardView, boardGame);
+    this.boardView = new BoardView();
+    this.hudView = new HudView();
+
+    initialize();
   }
 
-  public HBox getView(){
+  private void initialize() {
     boardContainer.getChildren().add(boardView.getView());
     hudContainer.getChildren().add(hudView.getView());
     StackPane.setAlignment(hudView.getView(), Pos.CENTER);
     HBox.setHgrow(hudContainer, Priority.ALWAYS);
 
     this.getChildren().setAll(boardContainer, hudContainer);
+  }
+
+  /**
+   * Gets the view of the game.
+   *
+   * @return the view as a Hbox.
+   */
+  public HBox getView() {
     return this;
+  }
+
+  /**
+   * Gets the view of the board.
+   *
+   * @return the view of board.
+   */
+  public BoardView getBoardView() {
+    return boardView;
+  }
+
+  /**
+   * Gets the view of the HUD.
+   *
+   * @return the view of HUD.
+   */
+  public HudView getHudView() {
+    return hudView;
   }
 }
