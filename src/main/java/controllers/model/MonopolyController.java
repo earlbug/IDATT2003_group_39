3e46db7c23fa662da12914caeb4fb59e5cba9bfd
Erlend <erlesu@stud.ntnu.no>
2025-05-02 15:11:40 +0200

    feat(controller): implement methods in MonopolyController

--- src/main/java/controllers/model/MonopolyController.java
+++ src/main/java/controllers/model/MonopolyController.java
@@ -1,5 +1,40 @@
 package controllers.model;
 
-public class MonopolyController {
+import interfaces.TileAction;
+import models.BoardGame;
+import models.Player;
+import models.actions.WinAction;
+import org.slf4j.Logger;
+
+public class MonopolyController extends GameController {
+
+  private final Logger logger = org.slf4j.LoggerFactory.getLogger(MonopolyController.class);
+
+  public MonopolyController(BoardGame boardGame) {
+    super(boardGame);
+  }
+
+  public void handleBancruptPlayerCheck(Player player) {
+    boardGame.removePlayer(player);
+  }
+
+  @Override
+  public boolean isWinConditionMet(Player player) {
+    logger.debug("Checking win condition for player {} on tile {}", player.getName(),
+        player.getCurrentTile().getTileId());
+    if (boardGame.getPlayers().size() == 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @Override
+  public void handlePlayerWinCheck() {
+    if (isWinConditionMet(boardGame.getCurrentPlayer())) {
+      Player victoryousPlayer = boardGame.getPlayers().getFirst();
+      notifyWinnerDetermined(victoryousPlayer);
+    }
+  }
 
 }
