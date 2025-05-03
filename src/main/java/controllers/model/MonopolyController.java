fffab160c436fb86fc43bf74da05a47d08dc62cd
Erlend <erlesu@stud.ntnu.no>
2025-05-03 14:23:09 +0200

    docs(MonopolyController): add javadoc

--- src/main/java/controllers/model/MonopolyController.java
+++ src/main/java/controllers/model/MonopolyController.java
@@ -6,14 +6,27 @@ import models.Player;
 import models.actions.WinAction;
 import org.slf4j.Logger;
 
+/**
+ * MonopolyController extends GameController and is responsible for controlling monopoly-specific game mechanics.
+ * It provides methods for checking if a player has lost, and if the winning condition for monopoly is met.
+ */
 public class MonopolyController extends GameController {
 
   private final Logger logger = org.slf4j.LoggerFactory.getLogger(MonopolyController.class);
 
+  /**
+   * Initializes a Monopoly game controller.
+   *
+   * @param boardGame the BoardGame to be played and controlled.
+   */
   public MonopolyController(BoardGame boardGame) {
     super(boardGame);
   }
 
+  /**
+   * checks if the current player has lost by being in debt,
+   * and if so removes the player form the boardgame.
+   */
   @Override
   public void handlePlayerLooseCheck() {
     Player currentPlayer = boardGame.getCurrentPlayer();
@@ -24,23 +37,19 @@ public class MonopolyController extends GameController {
     }
   }
 
+  /**
+   * An override of the super class method, to check if monopoly specific conditions are met for a
+   * player to be considered victorious.
+   * Specific conditions for monopoly is if there are only one player left.
+   *
+   * @param player Current player
+   * @return true if a winning condition is met, and false otherwise
+   */
   @Override
   public boolean isWinConditionMet(Player player) {
     logger.debug("Checking win condition for player {} on tile {}", player.getName(),
         player.getCurrentTile().getTileId());
-    if (boardGame.getPlayers().size() == 1) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  @Override
-  public void handlePlayerWinCheck() {
-    if (isWinConditionMet(boardGame.getCurrentPlayer())) {
-      Player victoryousPlayer = boardGame.getPlayers().getFirst();
-      notifyWinnerDetermined(victoryousPlayer);
-    }
+    return boardGame.getPlayers().size() == 1;
   }
 
 }
