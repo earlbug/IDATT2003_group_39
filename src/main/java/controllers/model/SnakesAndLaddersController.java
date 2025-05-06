6a168da46a8774d6edaeed69c9912042b13e6e5c
Erlend <erlesu@stud.ntnu.no>
2025-05-06 12:54:59 +0200

    refactor(controller): refactor snakes and ladders to implement loose and win condition

--- src/main/java/controllers/model/SnakesAndLaddersController.java
+++ src/main/java/controllers/model/SnakesAndLaddersController.java
@@ -1,6 +1,10 @@
 package controllers.model;
 
+import interfaces.TileAction;
+import java.util.List;
 import models.BoardGame;
+import models.Player;
+import models.actions.WinAction;
 import org.slf4j.Logger;
 
 /**
@@ -24,4 +28,31 @@ public class SnakesAndLaddersController extends GameController {
   }
 
 
+  /**
+   * An override of the super class method, to check if snakes and ladders
+   * specific conditions are met for a player to be considered a looser.
+   * Specific conditions for snakes and ladders are always false.
+   *
+   * @param player The player to check if the loose condition is met for.
+   * @return if the player fill the requirements for loosing
+   */
+  @Override
+  public boolean isLooseConditionMet(Player player) {
+    return false;
+  }
+
+  /**
+   * An override of the super class method, to check if snakes and ladders
+   * specific conditions are met for a player to be considered victorious.
+   * Specific conditions for snakes and ladders is if the player is standing on a winning Tile
+   *
+   * @param player Current player
+   * @return true if a winning condition is met, and false otherwise
+   */
+  @Override
+  public boolean isWinConditionMet(Player player) {
+    TileAction winningAction = boardGame.getBoard().getTile(player.getCurrentTile().getTileId())
+        .getLandAction();
+    return winningAction instanceof WinAction;
+  }
 }
