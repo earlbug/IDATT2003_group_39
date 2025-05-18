8c68a5d3b813097882825c7d7382a0c83c118845
torfoz <tord.fosse@gmail.com>
2025-05-18 23:23:00 +0200

    fix(JsonReader): Fixed JsonNull when setting nextTileId

--- src/main/java/IO/BoardFileReaderJson.java
+++ src/main/java/IO/BoardFileReaderJson.java
@@ -136,13 +136,11 @@ public class BoardFileReaderJson implements BoardFileReader {
     }
     // Gets the next tile as an int
     Map<Tile, Integer> tileMap;
-    int nextTileId;
-    if (tileJson.has("nextTileId")) {
+    int nextTileId = -1; // Default if no next tile
+    if (tileJson.has("nextTileId") && !tileJson.get("nextTileId").isJsonNull()) {
       nextTileId = tileJson.get("nextTileId").getAsInt();
-    } else {
-      // If the Tile has no nextTile, then set nextTileId to -1
-      nextTileId = -1;
     }
+
     tileMap = new HashMap<>();
     tileMap.put(tile, nextTileId);
 
@@ -267,9 +265,9 @@ public class BoardFileReaderJson implements BoardFileReader {
    * @return a SnakeAction object.
    */
   public SnakeAction deserializeSnakeAction(JsonObject snakeActionJson) {
-    int moneyDeducted = snakeActionJson.get("moneyDeducted").getAsInt();
+    int destinationTileId = snakeActionJson.get("destinationTileId").getAsInt();
 
-    SnakeAction snakeAction = new SnakeAction(moneyDeducted);
+    SnakeAction snakeAction = new SnakeAction(destinationTileId);
 
     if (snakeActionJson.has("description")) {
       snakeAction.setDescription(snakeActionJson.get("description").getAsString());
