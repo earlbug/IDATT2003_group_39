4a1ac7fabec4a08b7435020703846f7de3e95f06
Erlend <erlesu@stud.ntnu.no>
2025-05-19 18:16:41 +0200

    feat(viewa): add action tile view

--- src/main/java/views/content/ActionTileView.java
+++ src/main/java/views/content/ActionTileView.java
@@ -1,5 +1,28 @@
 package views.content;
 
-public class ActionTileView {
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
 
+public class ActionTileView extends Rectangle {
+
+
+  public ActionTileView() {
+    initialize();
+  }
+
+  private void initialize() {
+    this.setHeight(60);
+    this.setWidth(60);
+  }
+
+  public void setBackground(String fileName) {
+    Image image = new Image("resources/data/actionTileImages/" + fileName + ".png");
+    ImagePattern pattern = new ImagePattern(image);
+    this.setFill(pattern);
+  }
+
+  public Rectangle getView() {
+    return this;
+  }
 }
