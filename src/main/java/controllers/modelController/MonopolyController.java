a3d50f0e255557d4cea412ca26dd09202ab55b62
torfoz <tord.fosse@gmail.com>
2025-05-23 03:37:49 +0200

    method for clearing all observers

--- src/main/java/controllers/modelController/MonopolyController.java
+++ src/main/java/controllers/modelController/MonopolyController.java
@@ -15,10 +15,9 @@ import models.validators.ArgumentValidator;
 import org.slf4j.Logger;
 
 /**
- * MonopolyController extends GameController and is responsible for
- * controlling monopoly-specific game mechanics.
- * It provides methods for checking if a player has lost,
- * and if the winning condition for monopoly is met.
+ * MonopolyController extends GameController and is responsible for controlling monopoly-specific
+ * game mechanics. It provides methods for checking if a player has lost, and if the winning
+ * condition for monopoly is met.
  */
 public class MonopolyController extends GameController {
 
@@ -29,18 +28,15 @@ public class MonopolyController extends GameController {
 
   /**
    * An override of the super class method, to check if monopoly specific conditions are met for a
-   * player to be considered victorious.
-   * Specific conditions for monopoly is if there are only one player left,
-   * and it is the provided Player.
+   * player to be considered victorious. Specific conditions for monopoly is if there are only one
+   * player left, and it is the provided Player.
    *
    * @param player Current player
    * @return true if a winning condition is met, and false otherwise
    */
   @Override
   public boolean isWinConditionMet(Player player) {
-    List<Player> playersLeft = boardGame.getPlayers().stream()
-        .filter(p -> !p.hasLost())
-        .toList();
+    List<Player> playersLeft = boardGame.getPlayers().stream().filter(p -> !p.hasLost()).toList();
     return playersLeft.size() == 1 && playersLeft.getFirst().equals(player);
   }
 
@@ -107,6 +103,7 @@ public class MonopolyController extends GameController {
 
       MonopolyViewController viewController = (MonopolyViewController) viewManager.getCurrentViewController();
       viewController.setUpView(boardFileName);
+      clearObservers();
       addObserver(viewController);
 
       boardGame.createDice(2);
@@ -174,8 +171,8 @@ public class MonopolyController extends GameController {
 
   /**
    * An override of the super class method, to check if monopoly specific conditions are met for a
-   * player to be considered a looser.
-   * Specific conditions for monopoly is if The player is in debt.
+   * player to be considered a looser. Specific conditions for monopoly is if The player is in
+   * debt.
    *
    * @param player The player to check if the loose condition is met for.
    * @return if the player is in debt
--- src/main/java/controllers/modelController/SnakesAndLaddersController.java
+++ src/main/java/controllers/modelController/SnakesAndLaddersController.java
@@ -56,6 +56,7 @@ public class SnakesAndLaddersController extends GameController {
 
       SnakesAndLaddersViewController viewController = (SnakesAndLaddersViewController) viewManager.getCurrentViewController();
       viewController.setUpView(boardFileName, boardNumber);
+      clearObservers();
       addObserver(viewController);
 
       boardGame.createDice(1);
@@ -74,108 +75,108 @@ public class SnakesAndLaddersController extends GameController {
 
     // Notify observers about the move
     notifyPlayerMoved(currentPlayer, steps);
-}
-
-@Override
-public void handleOneTurn() {
-  int sum = handleRollDice();
-  handlePlayerMove(sum);
-  handlePlayerLandAction();
-  handlePlayerLooseCheck();
-  handlePlayerWinCheck();
-  handlePlayerTurnEnd();
-  handleNextPlayer();
-}
-
-private void handlePlayerLandAction() {
-  Player currentPlayer = boardGame.getCurrentPlayer();
-  TileAction tileAction = currentPlayer.getCurrentTile().getLandAction();
-  if (tileAction != null) {
-    tileAction.perform(currentPlayer);
-    logger.debug("Player {} performed action: {}", currentPlayer.getName(), tileAction);
-  } else {
-    logger.debug("No action for player {} on tile {}", currentPlayer.getName(),
-        currentPlayer.getCurrentTile().getTileId());
   }
-  notifyTileActionPerformed(currentPlayer);
-}
 
-private void handlePlayerTurnEnd() {
-  notifyTurnEnded(boardGame.getCurrentPlayer());
-}
-
-@Override
-public int handleRollDice() {
-  logger.debug("Rolling dice for player {}", boardGame.getCurrentPlayer().getName());
-  return boardGame.getDice().rollAllDice();
-}
-
-@Override
-public void handlePlayerLooseCheck() {
-// Not used
-}
-
-@Override
-public boolean isLooseConditionMet(Player player) {
-  // Not used
-  return false;
-}
-
-@Override
-public void handlePlayerWinCheck() {
-  if (isWinConditionMet(boardGame.getCurrentPlayer())) {
-    notifyWinnerDetermined(boardGame.getCurrentPlayer());
-    logger.debug("Player {} has won!", boardGame.getCurrentPlayer());
+  @Override
+  public void handleOneTurn() {
+    int sum = handleRollDice();
+    handlePlayerMove(sum);
+    handlePlayerLandAction();
+    handlePlayerLooseCheck();
+    handlePlayerWinCheck();
+    handlePlayerTurnEnd();
+    handleNextPlayer();
   }
-}
 
-@Override
-public boolean isWinConditionMet(Player player) {
-  TileAction winningAction = boardGame.getBoard().getTile(player.getCurrentTile().getTileId())
-      .getLandAction();
-  return winningAction instanceof WinAction;
-}
-
-@Override
-public void handleNextPlayer() {
-  boardGame.nextPlayer();
-  Player currentPlayer = boardGame.getCurrentPlayer();
-  while (currentPlayer.hasLost() || currentPlayer.getTurnsToSkip() > 0) {
-    if (currentPlayer.getTurnsToSkip() > 0) {
-      currentPlayer.addTurnsToSkip(-1);
+  private void handlePlayerLandAction() {
+    Player currentPlayer = boardGame.getCurrentPlayer();
+    TileAction tileAction = currentPlayer.getCurrentTile().getLandAction();
+    if (tileAction != null) {
+      tileAction.perform(currentPlayer);
+      logger.debug("Player {} performed action: {}", currentPlayer.getName(), tileAction);
+    } else {
+      logger.debug("No action for player {} on tile {}", currentPlayer.getName(),
+          currentPlayer.getCurrentTile().getTileId());
     }
+    notifyTileActionPerformed(currentPlayer);
+  }
+
+  private void handlePlayerTurnEnd() {
+    notifyTurnEnded(boardGame.getCurrentPlayer());
+  }
+
+  @Override
+  public int handleRollDice() {
+    logger.debug("Rolling dice for player {}", boardGame.getCurrentPlayer().getName());
+    return boardGame.getDice().rollAllDice();
+  }
+
+  @Override
+  public void handlePlayerLooseCheck() {
+// Not used
+  }
+
+  @Override
+  public boolean isLooseConditionMet(Player player) {
+    // Not used
+    return false;
+  }
+
+  @Override
+  public void handlePlayerWinCheck() {
+    if (isWinConditionMet(boardGame.getCurrentPlayer())) {
+      notifyWinnerDetermined(boardGame.getCurrentPlayer());
+      logger.debug("Player {} has won!", boardGame.getCurrentPlayer());
+    }
+  }
+
+  @Override
+  public boolean isWinConditionMet(Player player) {
+    TileAction winningAction = boardGame.getBoard().getTile(player.getCurrentTile().getTileId())
+        .getLandAction();
+    return winningAction instanceof WinAction;
+  }
+
+  @Override
+  public void handleNextPlayer() {
     boardGame.nextPlayer();
-    currentPlayer = boardGame.getCurrentPlayer();
+    Player currentPlayer = boardGame.getCurrentPlayer();
+    while (currentPlayer.hasLost() || currentPlayer.getTurnsToSkip() > 0) {
+      if (currentPlayer.getTurnsToSkip() > 0) {
+        currentPlayer.addTurnsToSkip(-1);
+      }
+      boardGame.nextPlayer();
+      currentPlayer = boardGame.getCurrentPlayer();
+    }
+
+    notifyNextPlayer(currentPlayer);
+    logger.debug("Next player: {}", currentPlayer.getName());
   }
 
-  notifyNextPlayer(currentPlayer);
-  logger.debug("Next player: {}", currentPlayer.getName());
-}
+  @Override
+  public void setUpGame() {
+    boardGame.setPlayerIds();
+    boardGame.addPlayersOnStartPos();
+    boardGame.setCurrentPlayer(boardGame.getPlayers().getFirst());
+    notifyGameStarted(boardGame);
+    logger.debug("Game fully set up");
+  }
 
-@Override
-public void setUpGame() {
-  boardGame.setPlayerIds();
-  boardGame.addPlayersOnStartPos();
-  boardGame.setCurrentPlayer(boardGame.getPlayers().getFirst());
-  notifyGameStarted(boardGame);
-  logger.debug("Game fully set up");
-}
+  @Override
+  public void setPlayers(Map<String, GamePiece> players) {
+    for (Map.Entry<String, GamePiece> entry : players.entrySet()) {
+      String playerName = entry.getKey();
+      GamePiece gamePiece = entry.getValue();
+      Player player = new Player(playerName, boardGame);
+      player.setGamePiece(gamePiece);
+      boardGame.addPlayer(player);
+      logger.debug("Player {} set up with game piece {}", playerName, gamePiece);
+    }
+  }
 
-@Override
-public void setPlayers(Map<String, GamePiece> players) {
-  for (Map.Entry<String, GamePiece> entry : players.entrySet()) {
-    String playerName = entry.getKey();
-    GamePiece gamePiece = entry.getValue();
-    Player player = new Player(playerName, boardGame);
-    player.setGamePiece(gamePiece);
-    boardGame.addPlayer(player);
-    logger.debug("Player {} set up with game piece {}", playerName, gamePiece);
+  @Override
+  public void setViewManager(ViewManager viewManager) {
+    this.viewManager = viewManager;
+    logger.debug("View manager set in GameController");
   }
 }
-
-@Override
-public void setViewManager(ViewManager viewManager) {
-  this.viewManager = viewManager;
-  logger.debug("View manager set in GameController");
-}
-  }
